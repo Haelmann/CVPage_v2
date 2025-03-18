242f94381906d57b9a4c4a8ec3644be7688ecac4
Ioann1742_desktop <ioann1742@gmail.com>
2025-03-18 16:43:47 +0200

    18.03

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const cells = [...document.querySelectorAll(".cell")];
-const score = { crossScore: 0, circleScore: 0 };
+const score = JSON.parse(localStorage.getItem("score"));
 const winPatterns = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8],
   [0, 4, 8], [2, 4, 6]
 ];
-const winnerText = document.getElementById("winnerText")
-const modal = document.getElementById("victory-window")
-const crossScore = document.getElementById("crossScore")
-const circleScore = document.getElementById("circleScore")
-const closeModal = document.getElementById("closeModal")
+const winnerText = document.getElementById("winnerText");
+const modal = document.getElementById("victory-window");
+const crossScore = document.getElementById("crossScore");
+const circleScore = document.getElementById("circleScore");
+const closeModal = document.getElementById("closeModal");
+const localStorageClear = document.getElementById("clearLocalScore");
+const myStorage = window.localStorage;
+
+if (!myStorage.getItem("score")) {
+  myStorage.setItem("score", JSON.stringify({ crossScore: 0, circleScore: 0 }));
+}
+
+crossScore.textContent = score.crossScore;
+circleScore.textContent = score.circleScore;
+
+
 
 console.log(cells);
 
@@ -61,15 +72,9 @@ function updateScore() {
     score.circleScore++
     circleScore.textContent = score.circleScore;
   }
+  myStorage.setItem("score", JSON.stringify(score));
 }
 
 document.getElementById("reset-button").addEventListener("click", resetGame);
+localStorageClear.addEventListener("click", () => {myStorage.clear(); document.location.reload()})
 closeModal.addEventListener("click", () => { modal.style.display = "none"; resetGame() })
-/* TODO:
-Check winner
-Modal window show - game reset - MW close
-score update
-score window between field and header
--------------
-LocalStorage
-*/
